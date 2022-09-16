122a261ef48f02f0c753534629a9a8a4525549a6
max <maxmario2001@gmail.com>
2022-09-16 23:42:58 +0530

    added category

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,24 +6,22 @@ const Header = () => {
     <header className='fixed z-50 w-screen  p-6 px-16'>
     {/* desktop & tab ,for mobile its hidden , for medium its set */}
 
-    <div className='hidden md:flex w-full h-full '> </div>  
+    <div className='hidden md:flex w-full h-full '>  
     <div className='flex items-center gap-2'>
         <img src={Logo} className='w-8 object-cover' alt='logo'/>
         <p className='text-headingColor text-xl front-bold'>City</p>
+        </div>
 
-
-
-
-    </div>
-
-    <ul className='flex items-center gap-8'>
-        <li>Home</li>
-        <li>Menu</li>
-        <li>About Us</li>
-        <li>Service</li>
+    <ul className='flex items-center gap-8 ml-auto'>
+        <li className='text-base text-textColor hover:text-headingColor'>Home</li>
+        <li className='text-base text-textColor hover:text-headingColor'>Menu</li>
+        <li className='text-base text-textColor hover:text-headingColor'>About Us</li>
+        <li className='text-base text-textColor hover:text-headingColor'>Service</li>
     </ul>
+    </div>
     {/* mobile */}
-    <div className='flex md:hidden w-full h-full  '> </div>  
+    <div className='flex md:hidden w-full h-full  '> 
+    </div>  
 
 
 
