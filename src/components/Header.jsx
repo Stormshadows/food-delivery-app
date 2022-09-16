c54d5790a6a4128a665d59052a6c823b5d4a9af8
max <maxmario2001@gmail.com>
2022-09-16 23:29:43 +0530

    added color + screen

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import Logo from "./img/logo.png";
 
 const Header = () => {
   return (
-    <div className='fixed z-50 w-screen bg-slate-300 p-6 px-16'>Header</div>
+    <header className='fixed z-50 w-screen  p-6 px-16'>
+    {/* desktop & tab ,for mobile its hidden , for medium its set */}
+
+    <div className='hidden md:flex w-full h-full '> </div>  
+    <div className='flex items-center gap-2'>
+        <img src={Logo} className='w-8 object-cover' alt='logo'/>
+        <p className='text-headingColor text-xl front-bold'>City</p>
+
+
+
+
+    </div>
+
+    <ul className='flex items-center gap-8'>
+        <li>Home</li>
+        <li>Menu</li>
+        <li>About Us</li>
+        <li>Service</li>
+    </ul>
+    {/* mobile */}
+    <div className='flex md:hidden w-full h-full  '> </div>  
+
+
+
+    </header>
   )
 }
 
