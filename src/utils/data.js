eadaf2b621e1eb23a5732e9744114e30b41764d7
max <maxmario2001@gmail.com>
2022-09-20 23:46:19 +0530

    added product cards with shadow

---
+++ src/utils/data.js
@@ -0,0 +1,23 @@
+import I1 from "../img/i1.png";
+import F1 from "../img/f1.png";
+import D3 from "../img/d3.png";
+import FI1 from "../img/fi1.png";
+
+export const herodata = [
+  {
+    id: 1,
+    name: "Icecream",
+    desc: "Vanilla & Chocolate",
+    price: "50",
+    imagesrc: I1,
+  },
+  { id: 2, name: "Fruits", desc: "Strawberry", price: "30", imagesrc: F1 },
+  {
+    id: 3,
+    name: "Beverages",
+    desc: "Monster Energy",
+    price: "90",
+    imagesrc: D3,
+  },
+  { id: 4, name: "Sushi", desc: "Ngiri ", price: "300", imagesrc: FI1 },
+];
