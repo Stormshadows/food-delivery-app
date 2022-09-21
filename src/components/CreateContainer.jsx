a929df4f2e54f01407a0687530bb92f9d808d3ac
max <maxmario2001@gmail.com>
2022-09-21 14:37:37 +0530

    created a container to upload items

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { MdFastfood } from "react-icons/md";
+import { categories } from "../utils/data";
+import Loader from "./Loader";
 
 const CreateContainer = () => {
   const [title, setTitle] = useState("");
@@ -11,10 +13,10 @@ const CreateContainer = () => {
   const [imageAsset, setImageAsset] = useState(null);
   const [alertStatus, setAlertStatus] = useState("danger");
   const [msg, setMsg] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   return (
     <div className="w-full min-h-screen flex items-center justify-center">
-      <div className="w-[90%] md:w-[75%] border border-gray-300 rounded-lg p-4 flex flex-col items-center justify-center">
+      <div className="w-[90%] md:w-[75%] border border-gray-300 rounded-lg p-4 flex flex-col items-center justify-center gap-4">
         {fields && (
           <motion.p
             initial={{ opacity: 0 }}
@@ -33,7 +35,28 @@ const CreateContainer = () => {
         <div className="w-full py-2 border-b border-gray-300 flex items-center gap-2">
           <MdFastfood className="text-xl text-gray-700" />
           <input type="text" required value={title} onChange={(e) => setTitle(e.target.value)} placeholder="title" 
-          className="w-full h-full text-lg bg-transparent font-semibold outline-none border-none placeholder:text-gray-500 text-textColor" />
+          className="w-full h-full text-lg bg-transparent font-semibold outline-none border-none placeholder:text-gray-500
+           text-textColor"  />
+        </div>
+        <div className="w-full">
+          <select className="outline-none w-full text-base border-b-2 border-gray-200 p-2 rounded-md cursor-pointer" 
+          onChange={(e) => setCategory(e.target.value)}>
+            <option value="other" className="bg-white">
+              Select Category
+            </option>
+            {categories && categories.map(items => (
+              <option key={items.id} className="text-base border-0 outline-none capitalize bg-white text-headingColor" 
+              value={items.urlParamName}>
+                {items.name}
+              </option>
+            ))}
+
+          </select>
+        </div>
+        <div className="group flex justify-center items-center flex-col border-2 border-dotted border-gray-300 w-full h-225 md:h-20
+        cursor-pointer rounded">
+          {isLoading ? <Loader/> : <></>}
+
         </div>
       </div>
     </div>
