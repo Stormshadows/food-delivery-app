49e06ca868793070827dfca71667cc281859fa23
max <maxmario2001@gmail.com>
2022-09-22 16:47:17 +0530

    added cart container

---
+++ src/components/CartContainer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CartContainer = () => {
+  return (
+    <div className="fixed top-0 right-0  w-full md:w-375 h-screen bg-white drop-shadow-md flex flex-col">
+      CartContainer
+    </div>
+  );
+};
+
+export default CartContainer;
