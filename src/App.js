76db1bf3fe244ed0a7049e6a0d29765cf4abb936
max <maxmario2001@gmail.com>
2022-09-22 16:34:50 +0530

    imported menu container

--- src/App.js
+++ src/App.js
@@ -2,25 +2,30 @@ import React, { useEffect } from 'react';
 import {Route,Routes} from "react-router-dom";
 import { AnimatePresence } from 'framer-motion';
 
-import { CreateContainer, Header, MainContainer } from './components';
+import { CreateContainer, Header, MainContainer,MenuContainer } from './components';
 import { useStateValue } from './context/StateProvider';
 import { getAllFoodItems } from './utils/firebaseFunctions';
+import { actionType } from './context/reducer';
 
 
 const App = () => {
 
-  const [{}, dispatch] = useStateValue();
+  const [{fooditems}, dispatch] = useStateValue();
 
   const fetchData = async () => {
     await getAllFoodItems().then((data) => {
-      console.log(data)
+      // console.log(data)
+      dispatch({
+        type : actionType.SET_FOOD_ITEMS,
+        fooditems : data
+      });
     });
   };
 
   useEffect (() => {
     fetchData();
   },[]);
-  
+
   return (
 
     <AnimatePresence wait>
@@ -30,6 +35,7 @@ const App = () => {
       <main className='mt-14 md:mt-20 px-16 py-4 w-full '> 
         <Routes>
           <Route path="/" element={<MainContainer/>}/>
+          <Route path='/menu' element={<MenuContainer/>}/>
           <Route path="/createItem" element={<CreateContainer/>}/>
         </Routes>
 
