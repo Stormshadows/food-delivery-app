ae95e6ecd907035b8f672565923af354ed4850e3
max <maxmario2001@gmail.com>
2022-09-23 19:23:57 +0530

    removed unused elements

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -7,9 +7,9 @@ import CartContainer from "./CartContainer";
 const MainContainer = () => {
   const [{ fooditems, cartShow, cartItems }, dispatch] = useStateValue();
 
-  const [scrollValue, setscrollValue] = useState(0);
+  // const [scrollValue, setscrollValue] = useState(0);
 
-  useEffect(() => {}, [scrollValue]);
+  // useEffect(() => {}, [scrollValue]);
 
   return (
     <div className="w-full h-auto flex flex-col items-center justify-center overflow-x-hidden">
