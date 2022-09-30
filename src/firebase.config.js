9035a9559f7d9766238c046ea57a36461f82782d
max <maxmario2001@gmail.com>
2022-09-30 18:43:40 +0530

    changes to api config

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -3,13 +3,13 @@ import {getFirestore} from 'firebase/firestore';
 import {getStorage} from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC4V4BqcXC4yZg4LVuP6FSOC3JVO8hUZAQ",
-    authDomain: "foodapp-855d2.firebaseapp.com",
-    databaseURL: "https://foodapp-855d2-default-rtdb.firebaseio.com",
-    projectId: "foodapp-855d2",
-    storageBucket: "foodapp-855d2.appspot.com",
-    messagingSenderId: "275475435419",
-    appId: "1:275475435419:web:5325eab398397185239b3e"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
   };
 
   const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
